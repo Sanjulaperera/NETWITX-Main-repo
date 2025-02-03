ca5544062743aa88adb630fc2c01e8aecb2d1017
itsjv14 <87951049+Sanjulaperera@users.noreply.github.com>
2025-02-03 15:45:48 +1100

    env variables

--- app/firebase/config.ts
+++ app/firebase/config.ts
@@ -6,12 +6,12 @@ import { getAuth } from 'firebase/auth';
 
 // Your web app's Firebase configuration
 export const firebaseConfig = {
-  apiKey: "AIzaSyBbSsv5XP9fEpjGHhvFHfWsX_k07VRQ3u4",
-  authDomain: "netwitx-auth.firebaseapp.com",
-  projectId: "netwitx-auth",
-  storageBucket: "netwitx-auth.firebasestorage.app",
-  messagingSenderId: "301945849380",
-  appId: "1:301945849380:web:8a967546c92a58e4b57ab1"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
